8d65e81220fe00f686cb2e387148068407db09a2
1dragon <dnjsdyd159@naver.com>
2024-05-21 12:38:14 +0900

    입력한 값이 비었을 경우 400 코드 리턴

--- src/main/java/com/prj2spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring20240521/controller/board/BoardController.java
@@ -3,6 +3,7 @@ package com.prj2spring20240521.controller.board;
 import com.prj2spring20240521.domain.board.Board;
 import com.prj2spring20240521.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,15 @@ public class BoardController {
     private final BoardService service;
 
     @PostMapping("add")
-    public void add(@RequestBody Board board) {
+    public ResponseEntity add(@RequestBody Board board) {
+        // 제목, 본문, 작성자가 비어있으면 저장이 안되게 검사
+        if (service.validate(board)) {
+            service.add(board);
+            return ResponseEntity.ok().build();
+        } else {
+            // 입력한 값이 비었을 경우 400 코드를 리턴함
+            return ResponseEntity.badRequest().build();
+        }
 
-        service.add(board);
     }
 }
--- src/main/java/com/prj2spring20240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring20240521/service/board/BoardService.java
@@ -15,4 +15,18 @@ public class BoardService {
     public void add(Board board) {
         mapper.insert(board);
     }
+
+    public boolean validate(Board board) {
+        // 제목, 본문, 작성자가 비어있으면 리턴값을 false로 줌
+        if (board.getTitle() == null || board.getTitle().isBlank()) {
+            return false;
+        }
+        if (board.getContent() == null || board.getContent().isBlank()) {
+            return false;
+        }
+        if (board.getWriter() == null || board.getWriter().isBlank()) {
+            return false;
+        }
+        return true;
+    }
 }

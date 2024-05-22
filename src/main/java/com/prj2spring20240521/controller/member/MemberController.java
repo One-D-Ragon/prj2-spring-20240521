12082b70402fc3b4d65f679572e84976d0cffd01
1dragon <dnjsdyd159@naver.com>
2024-05-22 15:11:53 +0900

    회원 목록 보기

--- src/main/java/com/prj2spring20240521/controller/member/MemberController.java
+++ src/main/java/com/prj2spring20240521/controller/member/MemberController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/member")
@@ -49,5 +51,10 @@ public class MemberController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         */
+    }
+
+    @GetMapping("list")
+    public List<Member> list() {
+        return service.list();
     }
 }

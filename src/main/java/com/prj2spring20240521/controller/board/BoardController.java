f0428522cf57e2d4c50d362667dace56507c6636
1dragon <dnjsdyd159@naver.com>
2024-05-24 10:35:48 +0900

    자신의 글만 수정 가능

--- src/main/java/com/prj2spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring20240521/controller/board/BoardController.java
@@ -65,7 +65,12 @@ public class BoardController {
     }
 
     @PutMapping("edit")
-    public ResponseEntity edit(@RequestBody Board board) {
+    public ResponseEntity edit(@RequestBody Board board
+            , Authentication authentication) {
+        if (!service.hasAccess(board.getId(), authentication)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
         if (service.validate(board)) {
             service.edit(board);
             return ResponseEntity.ok().build();

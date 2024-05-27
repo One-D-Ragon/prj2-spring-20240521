ba764121614fc50e6d1b2ff46c1dcadd9ba3dc14
1dragon <dnjsdyd159@naver.com>
2024-05-27 10:15:21 +0900

    마지막 페이지 번호 구하기

--- src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
@@ -54,5 +54,10 @@ public interface BoardMapper {
             LIMIT #{offset}, 10
             """)
     List<Board> selectAllPaging(Integer offset);
+
+    @Select("""
+            SELECT COUNT(*) FROM board
+            """)
+    Integer countAll();
 }
 

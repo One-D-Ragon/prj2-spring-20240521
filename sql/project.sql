57538249dfd288305101c3221fd61c923e677885
1dragon <dnjsdyd159@naver.com>
2024-05-27 09:27:35 +0900

    페이지 번호

--- sql/project.sql
+++ sql/project.sql
@@ -68,3 +68,9 @@ CREATE TABLE authority
 
 INSERT INTO authority (member_id, name)
 VALUES (3, 'admin');
+
+# 게시물 여러개 입력
+INSERT INTO board
+    (title, content, member_id)
+SELECT title, content, member_id
+FROM board;

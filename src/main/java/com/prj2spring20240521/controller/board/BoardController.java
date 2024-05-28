43a3c5f8a9c81fdbf4da83052c9c90d5698fae16
1dragon <dnjsdyd159@naver.com>
2024-05-28 15:12:05 +0900

    지울 파일 목록 전송

--- src/main/java/com/prj2spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring20240521/controller/board/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -73,8 +74,13 @@ public class BoardController {
     }
 
     @PutMapping("edit")
-    public ResponseEntity edit(@RequestBody Board board
-            , Authentication authentication) {
+    public ResponseEntity edit(Board board,
+                               @RequestParam(value = "removeFileList[]", required = false) List<String> removeFileList,
+                               Authentication authentication) {
+
+        System.out.println("board = " + board);
+        System.out.println("removeFileList = " + removeFileList);
+
         if (!service.hasAccess(board.getId(), authentication)) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }

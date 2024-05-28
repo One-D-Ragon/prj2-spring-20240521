832df27b32a0e2d7b3e19eb81b76f9905cf7d9c8
1dragon <dnjsdyd159@naver.com>
2024-05-28 11:36:10 +0900

    첨부된 이미지 출력

--- src/main/java/com/prj2spring20240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring20240521/service/board/BoardService.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -94,7 +95,16 @@ public class BoardService {
     }
 
     public Board get(Integer id) {
-        return mapper.selectById(id);
+        Board board = mapper.selectById(id);
+        List<String> fileNames = mapper.selectFileNameByBoardId(id);
+        // http://172.27.128.1:8888/{id}/{name}
+        List<String> imageSrcList = fileNames.stream()
+                .map(name -> STR."http://172.27.128.1:8888/\{id}/\{name}")
+                .toList();
+
+        board.setImageSrcList(imageSrcList);
+        
+        return board;
     }
 
     public void remove(Integer id) {

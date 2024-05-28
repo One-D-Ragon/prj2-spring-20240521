a938f9cf525ac6eebb10511ad096c222a9cca644
1dragon <dnjsdyd159@naver.com>
2024-05-28 12:10:17 +0900

    파일있는 게시물 삭제

--- src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
@@ -113,5 +113,11 @@ public interface BoardMapper {
             WHERE board_id=#{boardId}
             """)
     List<String> selectFileNameByBoardId(Integer boardId);
+
+    @Delete("""
+            DELETE FROM board_file
+            WHERE board_id=#{boardId}
+            """)
+    int deleteFileByBoardId(Integer boardId);
 }
 
